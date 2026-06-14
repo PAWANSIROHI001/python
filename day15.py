bc4ce1717b469acc9a13dd0f3f8f1c9b544a4b24
PAWAN SIROHI <PAWANSIROHI001@users.noreply.github.com>
2026-06-14 23:07:32 +0530

    Learn Python

---
+++ day15.py
@@ -0,0 +1,14 @@
+# strings funtions in python
+
+para = "hey my name is {} and i am from {}"
+
+name= "sirohi"
+country = "india"
+
+print(para.format(country, name))
+
+# using f strings
+print(f"hey my name is {name} and i am from {country}") #it will arrange the format
+
+print(type(f"{2 * 40}"))
+
